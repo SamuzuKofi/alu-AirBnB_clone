6e8704682a96bad51012a59965c6b26e8cf32b57
terrydestroye <s.amuzu@alustudent.com>
2025-09-14 17:36:57 +0200

    Added test code

--- console.py
+++ console.py
@@ -1,1 +1,23 @@
+import cmd
+import sys
 
+class MyCommandLine(cmd.Cmd):
+    intro = 'Welcome to MyCommandLine. Type help or ? to list commands.\n'
+    prompt = '(mycli) '
+
+    def do_greet(self, name):
+        """Greet the person with the given name."""
+        if name:
+            print(f"Hello, {name}!")
+        else:
+            print("Hello!")
+
+    def do_exit(self, arg):
+        """Exit the command line."""
+        print("Goodbye!")
+        return True
+
+    def do_EOF(self, arg):
+        """Handle EOF to exit."""
+        print("Goodbye!")
+        return True
